f1d3ec57f319286c5788def88be5dfc765fcc22e
Kun Syuu <kunsyuu@gmail.com>
2025-06-22 11:43:58 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,8 @@
 import Header from '@/components/Header/Header.vue';
 import Footer from '@/components/Footer/Footer.vue';
 import { Toaster } from '@/components/ui/toast';
-
+</script>
+<script>
 export default {
   name: 'Home',
   data() {
